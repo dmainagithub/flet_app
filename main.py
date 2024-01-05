76e2eb4b194f288168a6d890c848f89866549697
dmainagithub <maina2007wambugu@gmail.com>
2024-01-05 22:17:27 +0300

    Second commit

--- main.py
+++ main.py
@@ -12,23 +12,54 @@ def main(page: Page):
     FG = '#3450a1'
     PINK = '#eb06ff'
 
+    first_page_contents = Container(
+        content=Column(
+            controls=[
+                Row(alignment='spaceBetween',controls=[
+                        Container(
+                            content=Icon(
+                                icons.MENU)),
+                        Row(
+                            controls=[
+                                Icon(icons.SEARCH),
+                                Icon(icons.NOTIFICATIONS_OUTLINED)
+                            ]
+                        )
+                    ]),
+
+                Text(
+                    value='What\'s up, Olivia!'
+                )
+            ]
+        )
+    )
+
+    page_1 = Container()
+    page_2 = Row(
+        controls=[
+            Container(
+                width=400, height=850, bgcolor=FG, border_radius=35,
+                padding=padding.only(top=50, left=20, right=20, bottom=5),
+                content=Column(
+                    controls=[
+                        first_page_contents
+                    ]
+                )
+            )
+        ]
+    )
+
     container = Container(
-        width=500, height=500, bgcolor='red'
+        width=400, height=850, bgcolor=BG, border_radius=35,
+        content=Stack(
+            controls=[
+                page_1,
+                page_2
+            ]
+        )
     )
     page.add(container)
 
 
 app(target=main)
 
-'''
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-'''
